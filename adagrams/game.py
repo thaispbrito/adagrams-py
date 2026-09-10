76acb5f3c1abb1b6c2811cf9413e778ca4c49421
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-09 19:47:03 -0700

    Complete wave 1 by implementing the draw_letters() function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,30 @@
 from random import randint
 
 def draw_letters():
-    pass
+
+    letters = [
+        "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", 
+        "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", 
+        "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", 
+        "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", 
+        "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", 
+        "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", 
+        "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"
+    ]
+
+    hand = []
+    n = 98
+
+    while len(hand) < 10:
+
+        random_num = randint(1, n)
+        index = random_num - 1
+
+        hand.append(letters[index])
+        letters.remove(letters[index])
+        n -= 1
+
+    return hand
 
 def uses_available_letters(word, letter_bank):
     pass
