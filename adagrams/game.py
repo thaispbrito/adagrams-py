7bdd42ed88709c18814a716af10f28f396677934
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-13 17:04:16 -0700

    Refactor the function draw_letters using a dictionary instead of a list

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,15 +2,34 @@ from random import randint
 
 def draw_letters():
 
-    letters = [
-        "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", 
-        "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", 
-        "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", 
-        "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", 
-        "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", 
-        "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", 
-        "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"
-    ]
+    letters_distribution = {
+        'A': 9, 
+        'B': 2, 
+        'C': 2, 
+        'D': 4, 
+        'E': 12, 
+        'F': 2, 
+        'G': 3, 
+        'H': 2, 
+        'I': 9, 
+        'J': 1, 
+        'K': 1, 
+        'L': 4, 
+        'M': 2, 
+        'N': 6, 
+        'O': 8, 
+        'P': 2, 
+        'Q': 1, 
+        'R': 6, 
+        'S': 4, 
+        'T': 6, 
+        'U': 4, 
+        'V': 2, 
+        'W': 2, 
+        'X': 1, 
+        'Y': 2, 
+        'Z': 1
+    }
 
     hand = []
     n = 98
@@ -18,11 +37,18 @@ def draw_letters():
     while len(hand) < 10:
 
         random_num = randint(1, n)
-        index = random_num - 1
+        temp = 0
 
-        hand.append(letters[index])
-        letters.remove(letters[index])
-        n -= 1
+        for letter, count in letters_distribution.items():
+
+            temp += count
+
+            if temp >= random_num:
+
+                hand.append(letter)
+                letters_distribution[letter] -= 1
+                n -= 1
+                break
 
     return hand
 
@@ -122,7 +148,6 @@ def get_highest_word_score(word_list):
                 word_names_ties.append(word_names[i])
                 word_scores_ties.append(word_scores[i])
                 word_lengths_ties.append(word_lengths[i])
-
 
         if 10 in word_lengths_ties:
             for i in range(len(word_names_ties)):
